c9bcd95c9bedd914c455c0b329fc8a2d62b82261
10389030 <xu_jun_wei@126.com>
2013-04-30 11:05:48 +0800

    modified

--- site/basic/school.php
+++ site/basic/school.php
@@ -59,24 +59,56 @@ class School extends Actor
 
 	public function getJob($sid)
 	{
+		$query_str = "select aid, aname, Activity.cid, cname 
+					  from Activity, Comity
+					  where Activity.cid = Comity.cid AND aid in (
+							select aid 
+					  		from Apply, User
+							where Apply.uid = User.uid AND User.sid = $sid 
+					  )
+					  group by aid";
+		$result = $this->mysqli->query( $query_str );
+
+		$ret = array();
+		if( $result->error ){
+			while( $row = $result->fetch_assoc() ){
+				$ret[] = $row;
+			}
+			$result->close();
+		}
+		else{
+			die("Error while processing query: ".$result->error);
+		}
+	
+		return $ret;
 	}
 
 	public function approveActivity($sid, $aid)
 	{
+		$query_str = "insert into Autho(aid, sid) values($aid, $sid)";
+		
+		$ret['result'] = $this->mysqli->query( $query_str );
+
+		return $ret;
 	}
 
 	public function denyActivity( $sid, $aid)
 	{
+		$query_str = "delete from Autho where sid=$sid and aid=$aid";
+		
+		$ret['result'] = $this->mysqli->query( $query_str );
+
+		return $ret;
 	}	
 }
 
-$school  =  new School();
-$parameter = array(
-		'sname' => "xjw2",
-		'password' => "xjw"
-	);
-var_dump( $school->register( $parameter ) );
-var_dump( $school->login( $parameter ) );
-echo json_encode($school->getAllSchool( $parameter ) );
+# $school  =  new School();
+# $parameter = array(
+# 		'sname' => "xjw2",
+# 		'password' => "xjw"
+# 	);
+# var_dump( $school->register( $parameter ) );
+# var_dump( $school->login( $parameter ) );
+# echo json_encode($school->getAllSchool( $parameter ) );
 
 ?>
