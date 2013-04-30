64ed4e0a1929e2cc09f7583fae0b281923231917
10389030 <xu_jun_wei@126.com>
2013-04-30 19:42:35 +0800

    new

---
+++ site/register.php
@@ -0,0 +1,31 @@
+<?php
+require_once(dirname(__FILE__).'/basic/tools.php');
+require_once(dirname(__FILE__).'/basic/user.php');
+require_once(dirname(__FILE__).'/basic/school.php');
+require_once(dirname(__FILE__).'/basic/comity.php');
+
+if( !isset($_REQUEST['utype'] ) ) {
+	die('Illegal Operation');
+}
+
+$operator = null;
+switch( $_REQUEST('utype') )
+{	
+case 'user':
+	$operator = new User();
+	break;
+case 'comity':
+	$operator = new Comity();
+	break;
+case 'school':
+	$operator = new School();
+	break:
+default:
+	die('Illegal User Type');
+	break;
+}
+
+unset($_REQUEST['utype']);
+$result = $operator->register( $_REQUEST );
+echo json_encode( $result );
+?>
